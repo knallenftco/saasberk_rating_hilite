92e4cb73f8870c47a9ac94b92711f4dd875b433b
saasbook <saasbook@saasbook.(none)>
2012-10-19 17:52:56 -0700

    HW2 part 2 done onto the autograder!

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,11 +7,41 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order(params[:sort])
-    @sort_var=params[:sort]
+    #Getting the ratings for the form
     @all_ratings=Movie.get_ratings
-  end
+    @r=Hash.new
+    #Reset all check marks values to false, rely on params[:ratings]
+    if (params[:ratings]==nil)
+      @all_ratings.each do |i|
+        @r[i]=true
+        end
+    else
+      @all_ratings.each do |f|
+        @r[f]=false
+        end
+    end
+    #Sorting of original display list with all ratings check marked 
+    @sort_var=params[:sort]
 
+    #Handling check marked boxes ONLY
+    unless(params[:ratings]==nil)#&&params[:commit]==nil)
+         @movies_presort=Movie.find_all_by_rating(params[:ratings].keys)
+     if (params[:sort]=='title')
+         @movies=@movies_presort.sort_by { |t| t.title}
+     elsif (params[:sort]=='release_date')
+       @movies=@movies_presort.sort_by { |t| t.release_date}
+     else 
+       @movies=@movies_presort
+     end
+    #write keys out for final display
+      params[:ratings].keys.each do |t|
+        @r[t]=true
+    end
+    else
+    @movies = Movie.order(params[:sort])
+    end
+  end
+  
   def new
     # default: render 'new' template
   end
@@ -41,4 +71,3 @@ class MoviesController < ApplicationController
   end
 
 end
-
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,7 @@ def self.get_ratings
   @find_ratings=[]
   Movie.all.each do |m|
     @find_ratings<<m.rating
+    @r=Hash.new
   end
   return @find_ratings.sort.uniq
 end
